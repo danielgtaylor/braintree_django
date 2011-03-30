45259b97235dfd2f15d837b391d2ef96ad360dcd
Dmitry Shevchenko <dmishe@gmail.com>
2011-03-30 21:03:09 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-braintree',
-    version="1.3.1",
+    version="1.3.2",
     description='Django app for interacting with the braintree API',
     author='Daniel Taylor',
     author_email='dan@programmer-art.org',
